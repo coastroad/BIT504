3514e7c47acdfff690d839a62d26b7ebda0b546b
coastroad <coastroadhotsauce@gmail.com>
2022-04-06 17:02:48 +1200

    Added counter

---
+++ MyFirstProject/src/counter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class counter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter a number: ");
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+			System.out.println("\n" + i);
+		}
+		
+		scanner.close();
+
+	}
+
+}
